49b3acafc91cb571d4d28ea5526bd08f8659b2ee
Ram Patel <rr7patel@gmail.com>
2017-01-11 23:14:21 -0500

    Made Changes to urls.py and views
    Views now takes in request AND url as request is only POST or GET
    
    Fixed urls.py so that a url like veritas1.heroku.com/inserturlhere/
    passes inserturlhere to test function

--- verifyer/urls.py
+++ verifyer/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.test, name='verifyLink')
+    url(r'^/(?P<url>.+)/$', views.test, name='verify-url')
 ]
--- verifyer/views.py
+++ verifyer/views.py
@@ -5,9 +5,9 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 import verify
 # Create your views here.
-def test(request):
-    output = verify.verifyLink("Facebook.com")
+def test(request, url):
+    output = verify.verifyLink(url)
     if request.method == 'POST':
         return HttpResponse(output, content_type="text/plain")
     else:
-       return HttpResponse(output, content_type="text/plain")
+        return HttpResponse(output, content_type="text/plain")
