24de7e72371abc4b2d8676aaa80f4f0590f85e3f
Ram Patel <rr7patel@gmail.com>
2017-01-13 12:47:57 -0500

    Fixed Syntax

--- verifyer/urls.py
+++ verifyer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # Example: veritas1.herokuapp.com/content/facebook.com/
-    url(r'^get/$', views.test)
+    url(r'^get/$', views.test),
     # Example: vertias1.herokuapp.com/
-    url(r'^', views.default)
+    url(r'^$', views.default)
 ]
--- verifyer/views.py
+++ verifyer/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.core import serializers
 import verify
